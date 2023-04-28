c3a4ed88e2674f6c3583433a53d98a4b20688708
U-ma-s <yuma.prodia4989@gmail.com>
2023-04-28 23:34:50 +0900

    Initial Commit

---
+++ Drink Handy/Drink_HandyApp.swift
@@ -0,0 +1,18 @@
+//
+//  Drink_HandyApp.swift
+//  Drink Handy
+//  
+//  Created by umas on 2023/04/28
+//  
+//
+
+import SwiftUI
+
+@main
+struct Drink_HandyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
